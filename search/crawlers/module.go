fdcf10ddc0557544ff22ddd3113a52437750d7d5
Russell Jones <499552+jonesrussell@users.noreply.github.com>
2024-11-27 07:09:29 +0000

    Implement Colly collector initialization in crawlers module
    - Added a new function to create a Colly collector with debugging enabled, enhancing the crawling capabilities.
    - Updated the dependency injection in the FX module to provide the new Colly collector, improving modularity and testability.
    - Renamed NewCrawlerFromConfig to NewCollyCrawler for clarity and consistency with the new implementation.

--- search/crawlers/module.go
+++ search/crawlers/module.go
@@ -1,14 +1,22 @@
 package crawlers
 
 import (
+	"github.com/gocolly/colly/v2"
+	"github.com/gocolly/colly/v2/debug"
 	"go.uber.org/fx"
 )
 
 var Module = fx.Module("crawlers",
 	fx.Provide(
+		func() *colly.Collector {
+			c := colly.NewCollector(
+				colly.Debugger(&debug.LogDebugger{}),
+			)
+			return c
+		},
 		NewConfig,
 		fx.Annotate(
-			NewCrawlerFromConfig,
+			NewCollyCrawler,
 			fx.As(new(Crawler)),
 		),
 	),
